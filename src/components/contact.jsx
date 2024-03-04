449e9d00e52ac046fa47e0739d3a49a981a5b4c6
Quannggg <nhieuvai@gmail.com>
2024-03-04 23:25:30 +0700

    new

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -153,9 +153,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2024 Design:{" Quannguyen "}
+            <a href="https://github.com/Quannggg" rel="nofollow">
+              github
             </a>
           </p>
         </div>
