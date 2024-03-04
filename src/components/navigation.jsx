3a646f9050acc17cee8fa8a3515293ae3dd4bf81
Quannggg <nhieuvai@gmail.com>
2024-03-04 22:17:13 +0700

    small change

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+            VAP
           </a>{" "}
         </div>
 
@@ -35,21 +35,6 @@ export const Navigation = (props) => {
             <li>
               <a href="#about" className="page-scroll">
                 About
-              </a>
-            </li>
-            <li>
-              <a href="#services" className="page-scroll">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#portfolio" className="page-scroll">
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href="#testimonials" className="page-scroll">
-                Testimonials
               </a>
             </li>
             <li>
